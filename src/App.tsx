a7a4e3f5e7efd338de4cfb2b97f20279335a76d1
tiago-mends <tiagomendesdev@gmail.com>
2024-07-31 20:05:14 -0300

    build(deps): create 1 file and update 3 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { useRef } from "react";
+
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  function searchCity() {
+    const inputElement = inputRef.current;
+
+    const city = inputElement.value;
+    const key = process.env.OPEN_WEATHER_MAP_API_KEY;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+  }
+
+  return (
+    <>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Digite o nome da cidade..."
+      />
+      <button onClick={searchCity}>Buscar</button>{" "}
+    </>
+  );
 }
