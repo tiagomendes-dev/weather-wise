7849461905b181502ccbf05bd69c3cc7f8c81ef0
tiago-mends <tiagomendesdev@gmail.com>
2024-08-07 22:49:42 -0300

    update 1 file and copy 1 file

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,10 @@
-import axios from "axios";
-import { LocateFixedIcon, SearchIcon } from "lucide-react";
-import { useRef, useState } from "react";
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
-
-import WeatherInfo from "./components/weather-info";
-import WeatherInfoWeekly from "./components/weather-info-weekly";
+import ViaCepFetcher from "./components/via-cep-fetcher";
 
 export default function App() {
-  const [weather, setWeather] = useState();
-  const [weatherWeekly, setWeatherWeekly] = useState();
-  const inputRef = useRef();
-
-  async function searchCity() {
-    const city = inputRef.current.value;
-    const key = "6ae055123713b56d612527525352a746";
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`;
-    const urlWeekly = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&lang=pt_br&units=metric`;
-
-    const apiDataWeather = await axios.get(url);
-    const apiInfoWeekly = await axios.get(urlWeekly);
-
-    setWeather(apiDataWeather.data);
-    setWeatherWeekly(apiInfoWeekly.data);
-  }
   return (
     <div className="space-y-4">
-      <header className="rounded-xl bg-white px-10 py-4">
-        <div className="flex items-center justify-between">
-          <h1 className="hidden text-xl font-bold text-blue-500 sm:block">
-            WeatherWise
-          </h1>
-
-          <div className="flex w-full items-center gap-4 sm:w-auto">
-            <div className="inline-flex w-full rounded-lg border sm:w-auto">
-              <button className="px-4" onClick={searchCity}>
-                <SearchIcon className="size-4 text-zinc-600/70" />
-              </button>
-              <input
-                ref={inputRef}
-                className="flex-1 outline-none"
-                type="text"
-                placeholder="Search location"
-              />
-              <button className="rounded-r-lg bg-zinc-400/50 px-2 py-2">
-                <LocateFixedIcon className="size-4" />
-              </button>
-            </div>
-          </div>
-        </div>
-      </header>
-
-      {weather && (
+      <ViaCepFetcher />
+      {/* {weather && (
         <div className="space-y-4">
           <div className="grid gap-4 xl:grid-cols-2">
             {weather && <WeatherInfo weather={weather} />}
@@ -80,7 +33,7 @@ export default function App() {
             </MapContainer>
           </div>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
